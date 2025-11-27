8ffe3f6e2e09f79b1ba69cab066fb54eb719f7bc
adammKass <adam.kascak@protonmail.com>
2025-11-28 00:32:43 +0100

    -deleted unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import Navbar from "./components/Navbar";
-import HeroTest from "./components/HeroTest";
 import Swiper from "./components/Swiper";
-import SwiperFixed from "./components/SwiperFixed";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
-      {/* <SwiperFixed></SwiperFixed> */}
       <Swiper></Swiper>
-      {/* <HeroTest></HeroTest> */}
     </>
   );
 }
